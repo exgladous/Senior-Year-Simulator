b36d5b1896bdaf4c3ee0aa75199bc98349d832bb
Matthew J Gao <mg00805@srcs.loc>
2018-03-29 12:24:08 -0700

    added happiness, began events, more info to user

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 var player = {
         slp: 0,
+        hap: 0,
     },
     day = 0;
 
@@ -12,11 +13,13 @@ function nextday() {
         generateclasses()
         showschedule();
         document.getElementById("main").hidden = false
-        endturn.textContent = "Next Turn"
+        document.getElementById("intro").hidden = true
+        endturn.textContent = "Next Day"
     }
 
     day++;
-    evaluatesleep();
+    classevents();
+    evalulatestatus();
     updatestats();
     updateheader();
 }
@@ -37,29 +40,23 @@ function generateclasses() {
         player.classes[i] = player.classes[j];
         player.classes[j] = temp;
     }
+    player.periodone = player.classes[0];
+    player.periodtwo = player.classes[1];
+    player.periodthree = player.classes[2];
+    player.periodfour = player.classes[3];
+    player.periodfive = player.classes[4];
 
-    if (player.classes.length == 5) {
-        player.periodone = player.classes[0];
-        player.periodtwo = player.classes[1];
-        player.periodthree = player.classes[2];
-        player.periodfour = player.classes[3];
-        player.periodfive = player.classes[4];
-    } else if (player.classes.length == 6) {
-        player.periodone = player.classes[0];
-        player.periodtwo = player.classes[1];
-        player.periodthree = player.classes[2];
-        player.periodfour = player.classes[3];
-        player.periodfive = player.classes[4];
-        player.periodsix = player.classes[5];
-    } else if (player.classes.length == 7) {
-        player.periodone = player.classes[0];
-        player.periodtwo = player.classes[1];
-        player.periodthree = player.classes[2];
-        player.periodfour = player.classes[3];
-        player.periodfive = player.classes[4];
-        player.periodsix = player.classes[5];
-        player.periodseven = player.classes[6];
-    }
+    player.gradeone = 0;
+    player.gradetwo = 0;
+    player.gradethree = 0;
+    player.gradefour = 0;
+    player.gradefive = 0;
+
+    player.preparedone = 1;
+    player.preparedtwo = 1;
+    player.preparedthree = 1;
+    player.preparedfour = 1;
+    player.preparedfive = 1;
 }
 
 /////////// daily functions //////////
@@ -69,8 +66,13 @@ var header = document.getElementById("header");
 function updateheader() {
     if (day != 0 && day % 5 == 0) {
         header.textContent = "Today is day " + day + ", it's also a Friday, you'll be less sleepy if you overstudy."
+    } else if (day % 2 == 0) {
+        header.textContent = "Today is day " + day + ", a B day. You have " + player.periodone + ", " + player.periodtwo + ", and " + player.periodthree + ".";
+        var daytype = "B";
     } else {
-        header.textContent = "Today is day " + day;
+        header.textContent = "Today is day " + day + ", an A day. You have " + player.periodfour + " and " + player.periodfive + "."
+        var daytype = "A";
+
     }
 }
 ////// status update /////
@@ -78,11 +80,34 @@ var stats = document.getElementById("stats");
 stats.setAttribute("style", "white-space: pre;");
 
 function updatestats() {
-    stats.textContent = "Sleepiness: " + player.slp;
+    stats.textContent = " Sleepiness: " + player.slp + "\r\n Happiness: " + player.hap + "\r\n Grades: " + player.periodone + ": " + player.gradeone + ", " + player.periodtwo + ": " + player.gradetwo + ", " + player.periodthree + ": " + player.gradethree + ", " + player.periodfour + ": " + player.gradefour + ", " + player.periodfive + ": " + player.gradefive;
 }
-///// sleep evaluation /////
-function evaluatesleep() {
+///// status evaluation /////
+function evalulatestatus() {
     var sleepamount = document.getElementById("one").checked + document.getElementById("two").checked + document.getElementById("three").checked + document.getElementById("four").checked + document.getElementById("five").checked;
-    console.log(sleepamount)
 
+    player.slp = player.slp + sleepamount - 1.5
+    if (day % 5 == 0) {
+        player.hap = player.hap + 1;
+        player.slp = player.slp - 1;
+    }
+
+    if (player.slp > 5) {
+        player.slp = 5;
+        player.hap = player.hap - 3
+    } else if (player.slp < 0) {
+        player.hap = player.hap + 1
+        player.slp = 0;
+    }
+
+    if (player.hap > 5) {
+        player.hap = 5
+    }
+}
+
+var events = document.getElementById("events");
+function classevents() {
+    if (day == 1) {
+        events.textContent = "Today is the first day, you have no homework, but you are unsettled by the beginning of the end."
+    }
 }
